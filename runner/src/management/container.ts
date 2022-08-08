903a248d3c0957faaea2f1a06abc92ae79c377a4
Arraying <paul.huebner@googlemail.com>
2022-08-08 15:02:24 +0200

    Add container timeout to runner.

--- runner/src/management/container.ts
+++ runner/src/management/container.ts
@@ -15,6 +15,17 @@ export interface ContainerRun {
 }
 
 /**
+ * Custom timeout error that is thrown when container runtime exceeds a limit.
+ */
+export class StageTimeoutError extends Error {
+    constructor() {
+        super("Stage timeout exceeded");
+        // JavaScript things...
+        Object.setPrototypeOf(this, StageTimeoutError.prototype);
+    }
+}
+
+/**
  * Runs the pipeline containers. 
  * The result of each pipeline will be re-used for the other.
  * @param client The Docker client.
@@ -95,45 +106,69 @@ export async function runContainers(client: Docker, request: RunRequest, volumeN
         }
         // Merge this with any custom user settings, if applicable.
         const overrideOptions = getOverrideConfig();
-        console.log(overrideOptions);
         const options = mergician(candorOption, overrideOptions);
-        console.log('aa')
-        console.log(options)
         // Create the container and run it.
         const container = await client.createContainer(options);
         // Add deleting the container to the cleanup task.
-        cleaner.addJob(async (): Promise<void> => await container.remove());
+        cleaner.addJob(async (): Promise<void> => await container.remove({
+            // Needed in case of timeouts or other errors.
+            force: true,
+        }));
         try {
             // Write logs.
             const logStream = await container.attach({stream: true, stdout: true, stderr: true});
             container.modem.demuxStream(logStream, log, log);
             // Put the rest in a try so the container can be cleaned on error.
             await container.start();
-            // Await the container's exit.
-            const exit = await container.wait();
-            const exitCode = exit.StatusCode;
-            // Write it.
-            logInfo(log, `Process exited with status code ${exitCode}`);
-            if (exitCode === 0) {
-                // Exit code 0, so everything went smoothly.
-                stageRuns.push({
-                    name: stageNames[index],
-                    status: "Passed",
-                    exitCode: exitCode,
-                });
-                // Only write successful containers.
-                lastSuccessfulContainer = container.id;
-            } else {
-                // Nonzero exit code, there is a failure.
-                stageRuns.push({
-                    name: stageNames[index],
-                    status: "Failed",
-                    exitCode: exitCode,
-                });
-                // As such, skip subsequent stages.
-                skip = true;
+            // Await the container's exit, or timeout.
+            const race = [container.wait()];
+            const containerTimeout = getTimeoutPromise();
+            // If a timeout is set, add this to the race.
+            if (containerTimeout) {
+                race.push(containerTimeout);
             }
-
+            // Try/catch the timeout.
+            try {
+                const exit = await Promise.race(race);
+                const exitCode = exit.StatusCode;
+                // Write it.
+                logInfo(log, `Process exited with status code ${exitCode}`);
+                if (exitCode === 0) {
+                    // Exit code 0, so everything went smoothly.
+                    stageRuns.push({
+                        name: stageNames[index],
+                        status: "Passed",
+                        exitCode: exitCode,
+                    });
+                    // Only write successful containers.
+                    lastSuccessfulContainer = container.id;
+                } else {
+                    // Nonzero exit code, there is a failure.
+                    stageRuns.push({
+                        name: stageNames[index],
+                        status: "Failed",
+                        exitCode: exitCode,
+                    });
+                    // As such, skip subsequent stages.
+                    skip = true;
+                }
+            } catch (error) {
+                // See if this error is expected/recoverable.
+                if (error instanceof StageTimeoutError) {
+                    // Handle the timeout.
+                    logInfo(log, `Process exceeded stage execution time limit`);
+                    // Write the failure.
+                    // The container does not need to be killed, the cleaner will remove it with force.
+                    stageRuns.push({
+                        name: stageNames[index],
+                        status: "Failed",
+                        exitCode: -3,
+                    });
+                } else {
+                    // Delegate the error to the main error handler.
+                    throw error;
+                }
+            }
         } catch (exception) {
             console.error(exception);
             // Here, an exception occurred in the actual running of the container.
@@ -171,4 +206,22 @@ export function getOverrideConfig(): Docker.CreateServiceOptions {
         console.log("Error decoding RUNNER_CONTAINER_CONFIG_B into UTF-8 JSON, is it correct?");
         return {};
     }
+}
+
+/**
+ * Gets the timeout promise that will reject after the given timeout in seconds.
+ * If the timeotu is misconfigured or set less than or equal to 0, this will return null.
+ * If this returns a promise, this promise will always reject.
+ * @returns A promise of the configured timeout, or null if it is misconfigured.
+ */
+export function getTimeoutPromise(): Promise<void> | null {
+    // Make sure there is a timeout set.
+    const timeout = parseInt(process.env.RUNNER_CONTAINER_TIMEOUT || "-1");
+    if (isNaN(timeout) || timeout <= 0) {
+        return null;
+    }
+    // Return the promise.
+    return new Promise((_, reject) => {
+        setTimeout(() => reject(new StageTimeoutError()), timeout * 1000);
+    });
 }
