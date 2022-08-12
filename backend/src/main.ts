ee1e6872cc3e6bb7021d1f12c0da707f490a2a5b
Arraying <paul.huebner@googlemail.com>
2022-08-12 20:23:45 +0200

    Close dashboard server when SIGINT is received.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,12 +8,21 @@ const WEB_ONLY = process.env.WEB_ONLY || false;
 // Initialize the database as the first thing.
 AppDataSource.initialize()
     .then(async () => {
+        // Serve the web server.
+        const server = app.listen(3000);
         // Run the command-line tool.
         if (WEB_ONLY != "true") {
-            mainMenuLoop();
+            mainMenuLoop()
+                // When this exits, the server should be killed too.
+                .then(() => {
+                    server.close();
+                })
+                // Generic error handling.
+                .catch((error) => {
+                    console.error(error);
+                });
         }
-        // Serve the web server.
-        app.listen(3000);
+
     })
     .catch((e) => {
         console.error("Could not setup database.");
