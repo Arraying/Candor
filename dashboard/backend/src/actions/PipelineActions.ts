0dc23b3545dcf86bb84212ba769a06f1250f3b7d
Arraying <paul.huebner@googlemail.com>
2022-08-13 12:40:37 +0200

    Move over pipeline management to new actions.

---
+++ dashboard/backend/src/actions/PipelineActions.ts
@@ -0,0 +1,188 @@
+import crypto from "crypto";
+import prompts, { PromptObject } from "prompts";
+import { Pipeline } from "../entities/Pipeline";
+import { User } from "../entities/User";
+import { PipelineService } from "../services/PipelineService";
+import { UserService } from "../services/UserService";
+import { promptList, unanswered } from "./actions-utils";
+import { BaseAction } from "./BaseAction";
+
+export class PipelineAction extends BaseAction<Pipeline> {
+
+    constructor() {
+        const service = new PipelineService();
+        super(service, "pipeline", 64);
+    }
+
+    protected prettyPrint(pipeline: Pipeline): void {
+        const label = pipeline.public ? "Public" : "Private";
+        // Get assignee names.
+        const names = pipeline.assignees.map((assignee: User): string => assignee.name).join(", ");
+        // Stringify and hack due to lack of replaceAll.
+        let stringPlan = JSON.stringify(pipeline.plan, null, 2).split("\n").join("\n    ");
+        console.log(`==> ID: ${pipeline.id}\n    Name: ${pipeline.name}\n    Status: ${label}\n    Assignees: ${names}\n    Token: ${pipeline.token}\n    Config:\n    ${stringPlan}`);
+    }
+
+    protected createPrompts(): PromptObject<string>[] {
+        return [promptVisibility()];
+    }
+
+    protected async createValidateInput(response: any): Promise<string | null> {
+        return null;
+    }
+
+    protected async createBuildEntity(response: any): Promise<Pipeline> {
+        const pipeline = new Pipeline();
+        pipeline.token = await randomToken();
+        pipeline.public = response.public;
+        return pipeline;
+    }
+
+    protected createInfoMessage(): string {
+        return "It can be managed on the web dashboard";
+    }
+
+    /**
+     * Updates an existing pipeline's visibility (public/private).
+     */
+    async actionUpdateVisibility(): Promise<void> {
+        await this.actionUpdate({
+            prompts: [promptVisibility()],
+            update: updateVisbility,
+        });
+    }
+
+    /**
+     * Updates an existing pipeline's token (rerolls it).
+     */
+    async actionUpdateToken(): Promise<void> {
+        await this.actionUpdate({
+            prompts: [],
+            update: updateToken,
+        });
+    }
+
+    /**
+     * Assigns a user to a pipeline.
+     */
+    async actionAssign(): Promise<void> {
+        const pipelines = await this._service.getAll();
+        if (pipelines === "error") {
+            console.log("An error occurred getting all pipelines.");
+            return;
+        }
+        if (pipelines.length === 0) {
+            console.log("There are no pipelines.");
+            return;
+        }
+        const userService = new UserService();
+        const users = await userService.getAll();
+        if (users === "error") {
+            console.log("An error occurred getting all users.");
+            return;
+        }
+        if (users.length === 0) {
+            console.log("There are no users");
+            return;
+        }
+        const names = pipelines.map((pipeline: Pipeline): string => pipeline.name);
+        let response = await prompts(promptList("Which pipeline should someone be added to?", names));
+        // Check for early return.
+        if (unanswered(response)) {
+            return;
+        }
+        // Get the pipeline by name.
+        const pipeline = await this._service.getOne(response.name);
+        // Ask for the user.
+        const userNames = users.map((user: User): string => user.name);
+        response = await prompts(promptList("Which user should be added to this pipeline?", userNames));
+        // Check for early returns.
+        if (unanswered(response)) {
+            return;
+        }
+        // Get the user by name.
+        const user = await userService.getOne(response.name);
+        // Add them.
+        pipeline.assignees.push(user);
+        const status = await this._service.update(pipeline);
+        console.log(status === "success" 
+            ? "The user has been assigned."
+            : "An error occurred assigning the user."
+        );
+    }
+
+    /**
+     * Unassigns a user from a pipeline.
+     */
+    async actionUnassign(): Promise<void> {
+        const pipelines = await this._service.getAll();
+        if (pipelines === "error") {
+            console.log("An error occurred getting all pipelines.");
+            return;
+        }
+        if (pipelines.length === 0) {
+            console.log("There are no pipelines.");
+            return;
+        }
+        const names = pipelines.map((pipeline: Pipeline): string => pipeline.name);
+        let response = await prompts(promptList("Which pipeline should someone be removed from?", names));
+        // Get the pipeline by name.
+        const pipeline = await this._service.getOne(response.name);
+        // Ask for the user.
+        const userNames = pipeline.assignees.map((user: User): string => user.name);
+        response = await prompts(promptList("Which user should be added to this pipeline?", userNames));
+        // Check for early returns.
+        if (unanswered(response)) {
+            return;
+        }
+        // Remove them.
+        pipeline.assignees = pipeline.assignees.filter((user: User): boolean => user.name !== response.name);
+        const status = await this._service.update(pipeline);
+        console.log(status === "success" 
+            ? "The user has been unassigned."
+            : "An error occurred unassigning the user."
+        );
+    }
+
+}
+
+/**
+ * Updates the visibility.
+ * @param pipeline The pipeline.
+ * @param response The response.
+ */
+async function updateVisbility(pipeline: Pipeline, response: any) {
+    pipeline.public = response.public;
+}
+
+/**
+ * Updates the token.
+ * @param pipeline The pipeline.
+ * @param _ The response, ignored.
+ */
+async function updateToken(pipeline: Pipeline, _: any) {
+    pipeline.token = await randomToken();
+}
+
+/**
+ * The prompt that asks for the pipeline visibility.
+ * @returns A prompt.
+ */
+function promptVisibility(): prompts.PromptObject {
+    return {
+        type: "toggle",
+        name: "public",
+        message: "Enter the desired visibility",
+        initial: false,
+        active: "Public",
+        inactive: "Private",
+    };
+}
+
+/**
+ * Generates a cryptographically secure random token.
+ * @returns A random token.
+ */
+async function randomToken(): Promise<string> {
+   return crypto.randomBytes(16).toString('hex');
+}
--- dashboard/backend/src/menu.ts
+++ dashboard/backend/src/menu.ts
@@ -1,10 +1,11 @@
-import { pipelineList, pipelineAdd, pipelinePublic, pipelineReroll, pipelineAssign, pipelineUnassign, pipelineDel } from "./actions/actions-pipeline";
 import prompts from "prompts";
 import { RunnerAction } from "./actions/RunnerAction";
 import { UserAction } from "./actions/UserAction";
+import { PipelineAction } from "./actions/PipelineActions";
 
-const userAction = new UserAction();
-const runnerAction = new RunnerAction();
+const pipeline = new PipelineAction();
+const user = new UserAction();
+const runner = new RunnerAction();
 
 /**
  * The main menu loop, this will ensure the menu is always shown.
@@ -32,21 +33,21 @@ async function mainMenu(): Promise<boolean> {
         name: "value",
         message: "Please select an action:",
         choices: [
-            { title: "List pipelines", description: "Lists all pipelines.", value: pipelineList },
-            { title: "New pipeline", description: "Creates a new pipeline.", value: pipelineAdd },
-            { title: "Modify pipeline visibility", description: "Modifies whether or not the pipeline is visible.", value: pipelinePublic },
-            { title: "Reroll pipeline token", description: "Re-generates a new token for the pipeline.", value: pipelineReroll },
-            { title: "Assign user to pipeline", description: "Gives a user access to the pipeline.", value: pipelineAssign },
-            { title: "Unassign user from pipeline", description: "Removes access of a user from a pipeline.", value: pipelineUnassign },
-            { title: "Delete pipeline", description: "Deletes an existing pipeline.", value: pipelineDel },
-            { title: "List users", description: "Lists all users.", value: () => userAction.actionList() },
-            { title: "New user", description: "Creates a new user.", value: () => userAction.actionCreate() },
-            { title: "Reset user password", description: "Resets an existing user's password.", value: () => userAction.actionUpdatePassword() },
-            { title: "Delete user", description: "Deletes an existing user.", value: () => userAction.actionDelete() },
-            { title: "List runners", description: "Lists all runners.", value: () => runnerAction.actionList() },
-            { title: "New runner", description: "Registers a new runner.", value: () => runnerAction.actionCreate() },
-            { title: "Modify runner host", description: "Changes the runner's host.", value: () => runnerAction.actionUpdateHost() },
-            { title: "Delete runner", description: "Deletes an existing runner.", value: () => runnerAction.actionDelete() },
+            { title: "List pipelines", description: "Lists all pipelines.", value: () => pipeline.actionList() },
+            { title: "New pipeline", description: "Creates a new pipeline.", value: () => pipeline.actionCreate() },
+            { title: "Modify pipeline visibility", description: "Modifies whether or not the pipeline is visible.", value: () => pipeline.actionUpdateVisibility() },
+            { title: "Reroll pipeline token", description: "Re-generates a new token for the pipeline.", value: () => pipeline.actionUpdateToken() },
+            { title: "Assign user to pipeline", description: "Gives a user access to the pipeline.", value: () => pipeline.actionAssign() },
+            { title: "Unassign user from pipeline", description: "Removes access of a user from a pipeline.", value: () => pipeline.actionUnassign() },
+            { title: "Delete pipeline", description: "Deletes an existing pipeline.", value: () => pipeline.actionDelete() },
+            { title: "List users", description: "Lists all users.", value: () => user.actionList() },
+            { title: "New user", description: "Creates a new user.", value: () => user.actionCreate() },
+            { title: "Reset user password", description: "Resets an existing user's password.", value: () => user.actionUpdatePassword() },
+            { title: "Delete user", description: "Deletes an existing user.", value: () => user.actionDelete() },
+            { title: "List runners", description: "Lists all runners.", value: () => runner.actionList() },
+            { title: "New runner", description: "Registers a new runner.", value: () => runner.actionCreate() },
+            { title: "Modify runner host", description: "Changes the runner's host.", value: () => runner.actionUpdateHost() },
+            { title: "Delete runner", description: "Deletes an existing runner.", value: () => runner.actionDelete() },
             { title: "Exit", description: "Shuts down the dashboard.", value: shutdown },
         ],
     });
