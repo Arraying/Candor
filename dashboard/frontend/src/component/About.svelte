c978b2d0fe06eaac048addd69c99b24ec4cc0694
Arraying <paul.huebner@googlemail.com>
2022-08-17 19:24:42 +0200

    Create site privacy policy, update about.

--- dashboard/frontend/src/component/About.svelte
+++ dashboard/frontend/src/component/About.svelte
@@ -3,38 +3,73 @@
     import Box from "./Box.svelte";
 </script>
 
-<Box title={"About"} subtitle={"Information surrounding Candor and its ecosystem"}>
-
+<Box title={"About"} subtitle={"What is Candor?"}>
+    <p>
+        <strong>Candor is a minimalistic containerized Continuous Integration ecosystem.</strong>
+    </p>
+    <p>
+        For questions about this particular site's contents or purpose, contact the site's webmaster.
+    </p>
+    <p>
+        For more information regarding Candor and its capabilities, visit the <a href="https://github.com/Arraying/Candor">GitHub repository</a>.
+    </p>
 </Box>
 
-<Box title={"Privacy Policy"} subtitle={"Last changed: never"}>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-    In justo nunc, faucibus sit amet nisi eget, imperdiet molestie leo. 
-    Ut quis vehicula justo. Etiam eget velit eget diam auctor pellentesque. 
-    Cras vel velit porttitor, tincidunt nisl nec, maximus ipsum. 
-    Vestibulum diam nisl, efficitur nec magna eget, eleifend semper massa. 
-    Phasellus enim odio, pulvinar commodo interdum eu, consectetur vitae risus. 
-    Donec orci sapien, tincidunt in justo molestie, dapibus finibus est. Nam sed ullamcorper turpis. 
-    Pellentesque sit amet lectus mollis, venenatis erat sit amet, facilisis nunc. 
-    Aliquam sagittis dictum nibh eget elementum. 
-    Aliquam lorem odio, dignissim id venenatis quis, elementum non nunc. 
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-    In hac habitasse platea dictumst. 
-    Etiam non lectus eu massa tempus molestie.
-</Box>
+<Box title={"Privacy Policy"} subtitle={"Last changed: 17.08.2022"}>
+    <div class="content">
+        <h1 class="is-size-5">What data is collected</h1>
+        <p>
+            No data is collected from any visitors.
+            There are no anonymized statistics.
 
-<Box title={"Terms of Service"} subtitle={"Last changed: never"}>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-    In justo nunc, faucibus sit amet nisi eget, imperdiet molestie leo. 
-    Ut quis vehicula justo. Etiam eget velit eget diam auctor pellentesque. 
-    Cras vel velit porttitor, tincidunt nisl nec, maximus ipsum. 
-    Vestibulum diam nisl, efficitur nec magna eget, eleifend semper massa. 
-    Phasellus enim odio, pulvinar commodo interdum eu, consectetur vitae risus. 
-    Donec orci sapien, tincidunt in justo molestie, dapibus finibus est. Nam sed ullamcorper turpis. 
-    Pellentesque sit amet lectus mollis, venenatis erat sit amet, facilisis nunc. 
-    Aliquam sagittis dictum nibh eget elementum. 
-    Aliquam lorem odio, dignissim id venenatis quis, elementum non nunc. 
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-    In hac habitasse platea dictumst. 
-    Etiam non lectus eu massa tempus molestie.
+        </p>
+    </div>
+    <div class="content">
+        <h1 class="is-size-5">Your data protection rights</h1>
+        <ul>
+            <li>The right to erase</li>
+            <li>The right to rectify</li>
+            <li>The right to erasure</li>
+            <li>The right to restrict processing</li>
+            <li>The right to object to processing</li>
+            <li>The right to data portability</li>
+        </ul>
+    </div>
+    <div class="content">
+        <h1 class="is-size-5">Cookies</h1>
+        <p>
+            Cookies are text files placed on your computer that can be used to perform certain tasks in a website effectively.
+            For further information, visit <a href="https://allaboutcookies.org">allaboutcookies.org</a>.
+        </p>
+    </div>
+    <div class="content">
+        <h1 class="is-size-5">How cookies are used</h1>
+        <p>
+            Cookies are used purely for functional purposes.
+            The cookie given identifies you to your account, in case you are logged in.
+            This means that you may interact with restricted components of the website, and remain logged in.
+        </p>
+    </div>
+    <div class="content">
+        <h1 class="is-size-5">The type of cookie used</h1>
+        <p>
+            Only purely functional cookies are used.
+            This means there are <strong>no</strong> cookies that track you (on other websites) or relate to ads.
+        </p>
+    </div>
+    <div class="content">
+        <h1 class="is-size-5">How to manage cookies</h1>
+        <p>
+            Browsers can be set to not accept cookies.
+            In this case, the login functionality as well as any restricted actions will not be available on this site.
+            To remove cookeis, simply log out and <a href="https://www.groovypost.com/howto/remove-specific-cookies-from-google-chrome/">remove any corresponding cookies</a>.
+        </p>
+    </div>
+    <div class="content">
+        <h1 class="is-size-5">Changes to the privacy policy</h1>
+        <p>
+            This privacy policy was last changed on the 17th of August 2022.
+            Please ensure to regularly check this privacy policy for any updates.
+        </p>
+    </div>
 </Box>
