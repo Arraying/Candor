1ee67f5ee547ae5d6d9728d514ebd1617e0612ff
Arraying <paul.huebner@googlemail.com>
2022-11-18 19:41:35 +0100

    Remove bootstrap.

--- bootstrap/startS3Server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-docker run -d --name s3server \
-  -v $(pwd)/cloudserver_config.json:/usr/src/app/config.json \
-  -e S3DATA=multiple \
-  -e S3BACKEND=file \
-  -e SCALITY_ACCESS_KEY_ID=username \
-  -e SCALITY_SECRET_ACCESS_KEY=password \
-  -p 9000:8000 \
-  zenko/cloudserver
